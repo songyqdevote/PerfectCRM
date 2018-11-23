5f331867346d432bc619d68ee19c16df8b2e6a9e
songyqdevote <songyqdevote@163.com>
2018-11-23 22:46:51 +0800

    admin.py

--- PerfectCRM/settings.py
+++ PerfectCRM/settings.py
@@ -75,12 +75,24 @@ WSGI_APPLICATION = 'PerfectCRM.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PerfectCRM',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
 }
+
 
 
 # Password validation
--- crm/models.py
+++ crm/models.py
@@ -28,137 +28,136 @@ class Customer(models.Model):
     consultant = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
     memo = models.TextField(blank=True,null=True)
     date = models.DateTimeField(auto_now_add=True)
-    class Tag(models.Model):
-        name = models.CharField(unique=True, max_length=32)
-        def __str__(self):
-            return self.name
-    class CustomerFollowUp(models.Model):
-        '''客户跟进表'''
+class Tag(models.Model):
+    name = models.CharField(unique=True, max_length=32)
+    def __str__(self):
+        return self.name
+class CustomerFollowUp(models.Model):
+    '''客户跟进表'''
+    customer = models.ForeignKey("Customer", on_delete=models.CASCADE)
+    content = models.TextField(verbose_name="跟进内容")
+    consultant = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
 
-        customer = models.ForeignKey("Customer", on_delete=models.CASCADE)
-        content = models.TextField(verbose_name="跟进内容")
-        consultant = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
+    intention_choices = ((0, '2周内报名'),
+                         (1, '1月内报名'),
+                         (2, '近期无报名计划'),
+                         (3, '已在其他机构报名'),
+                         (4, '已报名'),
+                         (5, '已拉黑'),
+    )
+    intention = models.SmallIntegerField(choices=intention_choices)
+    date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return "<%s : %s>" %(self.customer.qq, self.intention)
 
-        intention_choices = ((0, '2周内报名'),
-                             (1, '1月内报名'),
-                             (2, '近期无报名计划'),
-                             (3, '已在其他机构报名'),
-                             (4, '已报名'),
-                             (5, '已拉黑'),
-        )
-        intention = models.SmallIntegerField(choices=intention_choices)
-        date = models.DateTimeField(auto_now_add=True)
-        def __str__(self):
-            return "<%s : %s>" %(self.customer.qq, self.intention)
+class Course(models.Model):
+    '''课程表'''
+    name = models.CharField(max_length=64, unique=True)
+    price = models.PositiveSmallIntegerField()
+    period = models.PositiveIntegerField(verbose_name="周期（月）")
+    outline = models.TextField()
 
-    class Course(models.Model):
-        '''课程表'''
-        name = models.CharField(max_length=64, unique=True)
-        price = models.PositiveSmallIntegerField()
-        period = models.PositiveIntegerField(verbose_name="周期（月）")
-        outline = models.TextField()
+    def __str__(self):
+        return self.name
+class Branch(models.Model):
+    '''校区'''
+    name = models.CharField(max_length=128, unique=True)
+    addr = models.CharField(max_length=128)
+    def __str__(self):
+        return self.name
 
-        def __str__(self):
-            return self.name
-    class Branch(models.Model):
-        '''校区'''
-        name = models.CharField(max_length=128, unique=True)
-        addr = models.CharField(max_length=128)
-        def __str__(self):
-            return self.name
+class ClassList(models.Model):
+    '''班级表'''
+    branch = models.ForeignKey("Branch",verbose_name="校区", on_delete=models.CASCADE)
+    course = models.ForeignKey("Course", on_delete=models.CASCADE)
+    class_type_choices = ((0, '面授（脱产）'),
+                          (1, '面授（周末）'),
+                          (2, '网络班'))
+    class_type = models.SmallIntegerField(choices=class_type_choices, verbose_name="班级类型")
+    semester = models.PositiveIntegerField(verbose_name="学期")
+    teachers = models.ManyToManyField("UserProfile")
+    start_date = models.DateField(verbose_name="开班日期")
+    end_date = models.DateField(verbose_name="结业日期", blank=True, null=True)
 
-    class ClassList(models.Model):
-        '''班级表'''
-        branch = models.ForeignKey("Branch",verbose_name="校区", on_delete=models.CASCADE)
-        course = models.ForeignKey("Course", on_delete=models.CASCADE)
-        class_type_choices = ((0, '面授（脱产）'),
-                              (1, '面授（周末）'),
-                              (2, '网络班'))
-        class_type = models.SmallIntegerField(choices=class_type_choices, verbose_name="班级类型")
-        semester = models.PositiveIntegerField(verbose_name="学期")
-        teachers = models.ManyToManyField("UserProfile")
-        start_date = models.DateField(verbose_name="开班日期")
-        end_date = models.DateField(verbose_name="结业日期", blank=True, null=True)
+    def __str__(self):
+        return "%s %s %s"%(self.branch, self.course, self.semester)
+    class Meta:
+        unique_together = ('branch', 'course', 'semester')
 
-        def __str__(self):
-            return "%s %s %s"%(self.branch, self.course, self.semester)
-        class Meta:
-            unique_together = ('branch', 'course', 'semester')
+class CourseRecord(models.Model):
+    '''上课记录'''
+    from_class = models.ForeignKey("ClassList", verbose_name="班级", on_delete=models.CASCADE)
+    day_num = models.PositiveIntegerField(verbose_name="第几节（天）")
+    teacher = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
+    has_homework = models.BooleanField(default=True)
+    homework_title = models.CharField(max_length=128, blank=True, null=True)
+    homework_content = models.TextField(blank=True, null=True)
+    outline = models.TextField(verbose_name="本节课程大纲")
+    date = models.DateField(auto_now_add=True)
+    def __str__(self):
+        return "%s %s"%(self.from_class, self.day_num)
+    class Meta:
+        unique_together = ("from_class", "day_num")
+class StudyRecord(models.Model):
+    '''学习记录'''
+    student = models.ForeignKey("Enrollment", on_delete=models.CASCADE)
+    course_record = models.ForeignKey("CourseRecord", on_delete=models.CASCADE)
+    attendance_choices = ((0, '已签到'),
+                          (1, '迟到'),
+                          (2, '缺勤'),
+                          (3, '早退'))
+    attendance =models.SmallIntegerField(choices=attendance_choices, default=0)
+    score_choice = ((100, "A+"),
+                    (90, "A"),
+                    (80, "B+"),
+                    (75, "B"),
+                    (70, "C+"),
+                    (-50, "D"),
+                    (-100, "copy"),
+                    (0, "N/A"))
+    score = models.SmallIntegerField(choices=score_choice, default=0)
+    memo = models.TextField(blank=True, null=True)
+    date = models.DateField(auto_now_add=True)
 
-    class CourseRecord(models.Model):
-        '''上课记录'''
-        from_class = models.ForeignKey("ClassList", verbose_name="班级", on_delete=models.CASCADE)
-        day_num = models.PositiveIntegerField(verbose_name="第几节（天）")
-        teacher = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
-        has_homework = models.BooleanField(default=True)
-        homework_title = models.CharField(max_length=128, blank=True, null=True)
-        homework_content = models.TextField(blank=True, null=True)
-        outline = models.TextField(verbose_name="本节课程大纲")
-        date = models.DateField(auto_now_add=True)
-        def __str__(self):
-            return "%s %s"%(self.from_class, self.day_num)
-        class Meta:
-            unique_together = ("from_class", "day_num")
-    class StudyRecord(models.Model):
-        '''学习记录'''
-        student = models.ForeignKey("Enrollment", on_delete=models.CASCADE)
-        course_record = models.ForeignKey("CourseRecord", on_delete=models.CASCADE)
-        attendance_choices = ((0, '已签到'),
-                              (1, '迟到'),
-                              (2, '缺勤'),
-                              (3, '早退'))
-        attendance =models.SmallIntegerField(choices=attendance_choices, default=0)
-        score_choice = ((100, "A+"),
-                        (90, "A"),
-                        (80, "B+"),
-                        (75, "B"),
-                        (70, "C+"),
-                        (-50, "D"),
-                        (-100, "copy"),
-                        (0, "N/A"))
-        score = models.SmallIntegerField(choices=score_choice, default=0)
-        memo = models.TextField(blank=True, null=True)
-        date = models.DateField(auto_now_add=True)
-
-        def __str__(self):
-            return "%s %s %s"%(self.student, self.course_record, self.score)
+    def __str__(self):
+        return "%s %s %s"%(self.student, self.course_record, self.score)
 
 
-    class Enrollment(models.Model):
-        '''报名表'''
-        customer = models.ForeignKey("Customer", on_delete=models.CASCADE)
-        enrolled_class = models.ForeignKey("ClassList", verbose_name="所报班级", on_delete=models.CASCADE)
-        consultant = models.ForeignKey("UserProfile", verbose_name="课程顾问", on_delete=models.CASCADE)
-        contract_agreed = models.BooleanField(default=False, verbose_name= "学员已同意合同")
-        contract_approved = models.BooleanField(default=False, verbose_name="合同已审核")
-        date = models.DateTimeField(auto_now_add=True)
-        def __str__(self):
-            pass
-            return "%s %s"  %(self.customer, self.enrolled_class)
-        class Meta:
-            unique_together = ("customer", "enrolled_class")
+class Enrollment(models.Model):
+    '''报名表'''
+    customer = models.ForeignKey("Customer", on_delete=models.CASCADE)
+    enrolled_class = models.ForeignKey("ClassList", verbose_name="所报班级", on_delete=models.CASCADE)
+    consultant = models.ForeignKey("UserProfile", verbose_name="课程顾问", on_delete=models.CASCADE)
+    contract_agreed = models.BooleanField(default=False, verbose_name= "学员已同意合同")
+    contract_approved = models.BooleanField(default=False, verbose_name="合同已审核")
+    date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        pass
+        return "%s %s"  %(self.customer, self.enrolled_class)
+    class Meta:
+        unique_together = ("customer", "enrolled_class")
 
-    class Payment(models.Model):
-        '''缴费记录'''
-        customer = models.ForeignKey("Customer", on_delete=models.CASCADE)
-        course = models.ForeignKey("Course",verbose_name="所报课程", on_delete=models.CASCADE)
-        amount = models.PositiveIntegerField(verbose_name="数额", default=500)
-        consultant = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
-        date = models.DateTimeField(auto_now_add=True)
+class Payment(models.Model):
+    '''缴费记录'''
+    customer = models.ForeignKey("Customer", on_delete=models.CASCADE)
+    course = models.ForeignKey("Course",verbose_name="所报课程", on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField(verbose_name="数额", default=500)
+    consultant = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
 
-        def __str__(self):
-            pass
-            return "%s %s" &(sel.customer,self.amout)
-    class UserProfile(models.Model):
-        '''账号表'''
-        user = models.OneToOneField(User, on_delete=models.CASCADE)
-        name = models.CharField(max_length=32)
-        roles = models.ManyToManyField("Role", blank=True, null=True)
+    def __str__(self):
+        pass
+        return "%s %s" &(sel.customer,self.amout)
+class UserProfile(models.Model):
+    '''账号表'''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=32)
+    roles = models.ManyToManyField("Role", blank=True, null=True)
 
-        def __str__(self):
-            return self.name
-    class Role(models.Model):
-        '''角色表'''
-        name = models.CharField(max_length=32, unique=True)
-        def __str__(self):
-            return self.name
+    def __str__(self):
+        return self.name
+class Role(models.Model):
+    '''角色表'''
+    name = models.CharField(max_length=32, unique=True)
+    def __str__(self):
+        return self.name
